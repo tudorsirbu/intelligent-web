fb41627ad4c7a597e20fc1c5a1869762a038a658
Cristian Gavrila <fcgavrila1@sheffield.ac.uk>
2014-03-08 20:04:34 +0000

    Initial.

---
+++ src/FourSquareTest.java
@@ -0,0 +1,85 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import fi.foyt.foursquare.api.*;
+import fi.foyt.foursquare.api.entities.*;
+
+public class FourSquareTest {
+
+	public void getLocationInformation(String shortURLs) {
+		
+		String clientID = "JRIYO5DDJ43NBLDNNU3PH1URI0RQ4ZA3TTHEDPFDPYOC2C5L";
+		String clientSecret = "QUIFUD44KU22UDMZUD0FQFRPWH21I2H4Y1J3CSWISUWOMZ1R";
+		String redirectUrl = "http://www.sheffield.ac.uk";
+		String accessToken = "5PJGTZWVP2QR1BK3LJHVLDYPQVFURSTUA1GGN0V3ZI2NBXIT";
+
+		FoursquareApi fsAPI = new FoursquareApi(clientID, clientSecret, redirectUrl);
+		fsAPI.setoAuthToken(accessToken);
+		String url = expandUrl(shortURLs);
+
+		//if it is not a 4square login url then we return!
+		if (!((url.startsWith("https://foursquare.com/"))&&(url.contains("checkin"))&&(url.contains("s=")))) 
+			return;
+		
+		//url now contains the full url!
+		Pattern pId = Pattern.compile(".+?checkin/(.+?)\\?s=.+", Pattern.DOTALL);
+		Matcher matche = pId.matcher(url);
+		String checkInId = (matche.matches()) ? matche.group(1) : "";
+		
+		Pattern pSig = Pattern.compile(".+?\\?s=(.*)\\&.+", Pattern.DOTALL);
+		matche = pSig.matcher(url);
+		String sig = (matche.matches()) ? matche.group(1) : "";
+		Result<Checkin> chck = null;
+
+		try {
+			chck = fsAPI.checkin(checkInId, sig);
+		} catch (FoursquareApiException e) {
+			e.printStackTrace(); 
+		}
+
+		Checkin cc = chck.getResult();
+		CompactUser user= cc.getUser();
+		System.out.print("CHECK IN: " + user.getFirstName() + " " + user.getLastName() + " (" + user.getGender());
+		System.out.print(") Just checked in at: ");
+		CompactVenue venue= cc.getVenue();
+		System.out.print(" " + venue.getName());
+		fi.foyt.foursquare.api.entities.Location loc= venue.getLocation();
+		System.out.print(" " + loc.getAddress() + " " + loc.getCity());
+		System.out.println(" " + loc.getLat() + ", " + loc.getLng());
+	}
+	
+	private String expandUrl(String shortURLs) {
+		String url = shortURLs;
+		String initialUrl = shortURLs;
+		while (url!=null){
+			try {
+				url = getFullURL(shortURLs);
+				if (url!=null) 
+					shortURLs= url;
+				else {
+					url= shortURLs;
+					break;
+				}
+			} catch (IOException e) {
+				// this is not a tiny URL as it is not redirected!
+				break;
+			}
+		}
+		
+		return url;
+	}
+	
+	private String getFullURL (String shortURLs) throws IOException {
+		URL shortUrl= new URL(shortURLs);
+		final HttpURLConnection httpURLConnection = (HttpURLConnection)shortUrl.openConnection();
+		httpURLConnection.setInstanceFollowRedirects(false);
+		httpURLConnection.connect();
+		final int responseCode = httpURLConnection.getResponseCode();
+		final String header = httpURLConnection.getHeaderField("Location");
+		return header;
+	}
+
+}
---
+++ src/GetTweetByLocation.java
@@ -0,0 +1,98 @@
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import twitter4j.GeoLocation;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class GetTweetByLocation {	
+	
+	public String getSimpleTimeLine(Twitter twitter){	
+		String resultString= "";	
+		FourSquareTest fs = new FourSquareTest();
+		Pattern p = Pattern.compile("\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+		
+		try {	
+			// it creates a query and sets the geocode	
+			//requirement	
+			Query query= new Query("#foursquare");	
+			query.setCount(10);
+//			query.setGeoCode(new GeoLocation(53.383, -1.483), 2, Query.KILOMETERS);	
+
+			//it fires the query	
+			QueryResult result = twitter.search(query);	
+			//it cycles on the tweets	
+			List<Status> tweets = result.getTweets();
+
+			for (Status tweet:tweets) {
+				///gets the user		
+								
+				User user = tweet.getUser();
+				
+				if (tweet.getText().toLowerCase().contains("foursquare") == true) {
+					Matcher m = p.matcher(tweet.getText());
+					
+					while (m.find()) {
+						fs.getLocationInformation(m.group());
+					}					
+				}
+				
+				Status status= (user.isGeoEnabled())? user.getStatus() : null;
+				if (status==null) 	
+					resultString+="@" + tweet.getText() + " (" + user.getLocation() + ") \n";	
+				else 
+					resultString+="@" + tweet.getText() + " (" + ((status!=null&&status.getGeoLocation()!=null) ? status.getGeoLocation().getLatitude() + "," + status.getGeoLocation().getLongitude():user.getLocation()) + ") \n";	
+
+			}	
+		} 
+		catch (Exception te) {	
+			te.printStackTrace(); 	
+			System.out.println("Failed to search tweets:" +
+					te.getMessage());	
+			System.exit(-1);	
+		}	
+		return resultString;	
+
+	}
+
+	private Twitter init() throws Exception{	
+		String consumerkey = "H4VHRaf8ybmPhzzK47uQ";	
+		String consumersecret = "y6oxNsvuoauf4sPcGU45Ct5eVfryYlai5TUBU92Uxbk";	
+		String accesstoken = "1225017144-1l22gHEw6SpxoQQac1PmT5a3FjQnexJrMQmiFra";	
+		String accesstokensecret = "WR2I8lHSBlqVKHV1a3t3CDElHKe0sHkVl1TCLyrVnrkLS";	
+		Twitter twitterConnection = initTwitter(consumerkey, 	
+				consumersecret, accesstoken, accesstokensecret);	
+		return twitterConnection;	
+	}
+
+	private Twitter initTwitter(String consumerKey, String consumerSecret, 	
+			String accessToken, String accessTokenSecret) 	
+					throws Exception {	
+		ConfigurationBuilder cb = new ConfigurationBuilder();	
+		cb.setDebugEnabled(true)	
+		.setOAuthConsumerKey(consumerKey)	
+		.setOAuthConsumerSecret(consumerSecret)	
+		.setOAuthAccessToken(accessToken)	
+		.setOAuthAccessTokenSecret(accessTokenSecret)	
+		.setJSONStoreEnabled(true);	
+		return (new TwitterFactory(cb.build()).getInstance());	
+	}
+
+	public static void main(String[] args) {	
+		GetTweetByLocation tt = new GetTweetByLocation();	
+		Twitter twitterConnection = null;	
+		try {	
+			twitterConnection= tt.init();		 	 	 	
+			System.out.print(tt.getSimpleTimeLine(twitterConnection));	
+		} catch (Exception e) {	
+			System.out.println("Cannot initialise Twitter");	
+			e.printStackTrace();	
+		}	
+	}
+}
