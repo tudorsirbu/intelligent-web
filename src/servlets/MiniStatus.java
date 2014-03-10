b409bdcedbbe4a9cf7b63b7b3b829f28936a4729
Tudor Sirbu <tudor.sirbu@gmail.com>
2014-03-10 02:39:50 +0000

    Created an object to store information about a status

---
+++ src/servlets/MiniStatus.java
@@ -0,0 +1,82 @@
+package servlets;
+import twitter4j.*;
+public class MiniStatus {
+	private String profileImageUrl;
+	private String name;
+	private String screenName;
+	private String text;
+	private String id;
+	private String retweetCount;
+	
+	public MiniStatus(Status status){
+		this.id = String.valueOf((status.getId()));
+		
+		// get the user
+		User u = status.getUser();
+		
+		
+		this.profileImageUrl = u.getMiniProfileImageURL();
+		this.name = u.getName();
+		this.screenName = u.getScreenName();
+		this.text = status.getText();
+		this.retweetCount = String.valueOf(status.getRetweetCount());
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
+	}
+
+	public void setProfileImageUrl(String profileImageUrl) {
+		this.profileImageUrl = profileImageUrl;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getScreenName() {
+		return screenName;
+	}
+
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRetweetCount() {
+		return retweetCount;
+	}
+
+	public void setRetweetCount(String retweetCount) {
+		this.retweetCount = retweetCount;
+	}
+
+	@Override
+	public String toString() {
+		return "MiniStatus [profileImageUrl=" + profileImageUrl + ", name="
+				+ name + ", screenName=" + screenName + ", text=" + text
+				+ ", id=" + id + ", retweetCount=" + retweetCount + "]";
+	}
+
+
+	
+}
