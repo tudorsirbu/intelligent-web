1e3dd65e8be8321030727ae88d356dc916657cd9
Tudor Sirbu <tudor.sirbu@gmail.com>
2014-03-14 20:16:29 +0000

    Added method to get user by id from the local db

--- src/model/UserService.java
+++ src/model/UserService.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -21,6 +23,28 @@ public class UserService {
 		// create a connection to the database
 		this.connection = connection;
 	}
+	
+	public User getUser(String id){
+		try {
+			String query = "SELECT * FROM users  WHERE `id`='?'";
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, id);
+			ResultSet results = preparedStatement.executeQuery(query);
+			while(results.next()){
+				String name = results.getString("name");
+				String username = results.getString("username");
+				String location = results.getString("location");
+				String description = results.getString("description");
+				String profilePicURL = results.getString("profilePictureURL");
+				return new User(id, name, username, location, description, profilePicURL, null);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return null;
+	}
+		
 	/**
 	 * The method inserts a user in the users table in the database
 	 * @param user the details for the user to be inserted in the database
@@ -33,6 +57,42 @@ public class UserService {
 		String location = user.getLocation();
 		String description = user.getDescription();
 		String profilePictureURL = user.getProfilePicURL();
+
+
+		try {
+			//  statement for query execution
+			statement = connection.createStatement();
+			// query 
+			String query = "INSERT INTO users (`id`, `name`, `username`, "+
+					"`location`, `description`, `profilePictureURL`) values('"+id+"',"
+					+ "'"+name+"','"+username+"','"+ location+"','"+ description+"','"+ profilePictureURL+"')";
+			// Updating Table
+			statement.executeUpdate(query);          
+		}
+		catch (Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	/**
+	 * The method inserts a user in the users table in the database
+	 * @param user the details for the user to be inserted in the database
+	 */
+	public void insertUser(twitter4j.User user){
+		// user details
+		String id = String.valueOf(user.getId());
+		String name = user.getName();
+		String username= user.getScreenName();
+		String location = user.getLocation();
+		String description = user.getDescription();
+		String profilePictureURL = user.getProfileImageURL();
 
 
 		try {
