9b5f5285d97863ebb42274ec9f6929e525f10746
Tudor Sirbu <tudor.sirbu@gmail.com>
2014-03-19 00:40:20 +0000

    Minor fixes

--- src/model/UserService.java
+++ src/model/UserService.java
@@ -77,12 +77,6 @@ public class UserService {
 		catch (Exception e) {
 			System.out.println(e.toString());
 			e.printStackTrace();
-		} finally {
-			try {
-				this.statement.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
@@ -119,13 +113,6 @@ public class UserService {
 		catch (Exception e) {
 			System.out.println(e.toString());
 			e.printStackTrace();
-		} finally {
-			try {
-				statement.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 }
