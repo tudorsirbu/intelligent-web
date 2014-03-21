beb46376a3d372baff204322d6b9bd88245a3254
CTarta <claudiu.tarta@gmail.com>
2014-03-21 20:30:35 +0000

    Fixed the maps

--- WebContent/javascript/script.js
+++ WebContent/javascript/script.js
@@ -82,7 +82,9 @@ $(document).ready(function() {
 	});
 
 	$("#userVenuesForm").submit(function( event ) {
+		clearOverlays();
 		google.maps.event.addDomListener(window, 'load', initMapEmpty);
+		
 		$("#results").hide(0);
 		$("#results").empty();
 
@@ -327,6 +329,13 @@ function displayTweets(data) {
 
 	});
 }
+var markersArray = [];
+function clearOverlays() {
+	  for (var i = 0; i < markersArray.length; i++ ) {
+	    markersArray[i].setMap(null);
+	  }
+	  markersArray.length = 0;
+	}
 
 var map;
 function initMapEmpty(){
@@ -355,6 +364,7 @@ function initMap(data){
 		      map: map,
 		      title: venue.name
 		  });
+		  markersArray.push(marker);
 		  google.maps.event.addListener(marker, 'click', function() {
 		    infowindow.open(map,marker);
 		  });
@@ -365,6 +375,7 @@ function initMap(data){
 
 function displayVenues(data){
 	google.maps.event.addDomListener(window, 'load', initMap(data));
+	
 	$("#results").show(0);
 	$.each(data, function( key, venue ) {
 		photoGroups = venue.photos.groups;
@@ -395,6 +406,7 @@ function displayVenues(data){
 
 function displayVenueStream(data) {
 	google.maps.event.addDomListener(window, 'load', initMap(data));
+	
 	$.each(data, function( key, venue ) {
 		if(data){
 			$("#map-canvas").show(0);
@@ -408,12 +420,13 @@ function displayVenueStream(data) {
 			if(venue.shortUrl)
 			div += "<h class='title'>Url:  </h><a target="+"'_blank'"+" href="+venue.shortUrl+">"+ venue.shortUrl + "</a><br/><br/>";
 			console.log("hhghh"+photoGroups);
-			if(photoGroups.length>0)
+			if(photoGroups.length>1)
 				if(photoGroups[1].length!=0)
 					$.each(photoGroups[1].items,function(key,value){
 						div += "<img src='"+ value.url +"' />";
 					});
 				else{
+					if(photoGroups.length>0)
 					if(photoGroups[0].length!=0)
 						$.each(photoGroups[0].items,function(key,value){
 							div += "<img src='"+ value.url +"' />";
