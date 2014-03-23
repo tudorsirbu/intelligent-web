3b8e1401dc8fa723742564074995e1890476b616
Cristian Gavrila <fcgavrila1@sheffield.ac.uk>
2014-03-23 20:35:20 +0000

    Slight design change for the forms and display of Venues for user

--- WebContent/javascript/script.js
+++ WebContent/javascript/script.js
@@ -393,12 +393,12 @@ function displayVenues(data){
 	$.each(data, function( key, venue ) {
 		photoGroups = venue.photos.groups;
 		var div = "<div class='venues'>";
-		div += "<h class='title'>" +"Name:  </h><span class='venues_name'>"+ venue.name + "</span><br/>";
-		div += "<h class='title'>Category:  </h><span class='venues_name'>"+ venue.categories[0].name + "</span><br/>";
+		div += "<h3 class='title'>Name:  </h3><span class='venues_name'>"+ venue.name + "</span><br/>";
+		div += "<h3 class='title'>Category:  </h3><span class='venues_name'>"+ venue.categories[0].name + "</span><br/>";
 		if(venue.location.address)
-		div += "<h class='title'>Address:  </h><span class='venues_name'>"+ venue.location.address + "</span><br/>";
+			div += "<h3 class='title'>Address:  </h3><span class='venues_name'>"+ venue.location.address + "</span><br/>";
 		if(venue.shortUrl)
-		div += "<h class='title'>Url:  </h><a href='url'>"+ venue.shortUrl + "</a><br/><br/>";
+			div += "<h3 class='title'>URL:  </h3><a href='url'>"+ venue.shortUrl + "</a><br/><br/>";
 		if(photoGroups.length>1)
 			if(photoGroups[1].length!=0)
 				$.each(photoGroups[1].items,function(key,value){
