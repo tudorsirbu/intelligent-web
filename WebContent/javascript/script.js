6d43de3147609e2291281c76ddc0685ee5fd3818
CTarta <claudiu.tarta@gmail.com>
2014-03-23 20:35:40 +0000

    Script js

--- WebContent/javascript/script.js
+++ WebContent/javascript/script.js
@@ -434,38 +434,47 @@ function displayVenues(data){
 }
 
 function displayVenueStream(data) {
-	google.maps.event.addDomListener(window, 'load', initMap(data));
-	
-	$.each(data, function( key, venue ) {
-		if(data){
-			$("#map-canvas").show(0);
-			$("#results").show(0);
-			var photoGroups = venue.photos.groups;
-			var div = "<div class='venues'>";
-			div += "<h class='title'>" +"Name:  </h><span class='venues_name'>"+ venue.name + "</span><br/>";
-			div += "<h class='title'>Category:  </h><span class='venues_name'>"+ venue.categories[0].name + "</span><br/>";
-			if(venue.location.address)
-			div += "<h class='title'>Address:  </h><span class='venues_name'>"+ venue.location.address + "</span><br/>";
-			if(venue.shortUrl)
-			div += "<h class='title'>Url:  </h><a target="+"'_blank'"+" href="+venue.shortUrl+">"+ venue.shortUrl + "</a><br/><br/>";
-			console.log("hhghh"+photoGroups);
-			if(photoGroups.length>1)
-				if(photoGroups[1].length!=0)
-					$.each(photoGroups[1].items,function(key,value){
-						div += "<img src='"+ value.url +"' />";
-					});
-				else{
-					if(photoGroups.length>0)
-					if(photoGroups[0].length!=0)
-						$.each(photoGroups[0].items,function(key,value){
+	if(data.lenth!=0){
+		google.maps.event.addDomListener(window, 'load', initMap(data));
+		
+		$.each(data, function( key, venue ) {
+			if(data){
+				$("#map-canvas").show(0);
+				$("#results").show(0);
+				var photoGroups = venue.photos.groups;
+				var div = "<div class='venues'>";
+				div += "<h class='title'>" +"Name:  </h><span class='venues_name'>"+ venue.name + "</span><br/>";
+				div += "<h class='title'>Category:  </h><span class='venues_name'>"+ venue.categories[0].name + "</span><br/>";
+				if(venue.location.address)
+				div += "<h class='title'>Address:  </h><span class='venues_name'>"+ venue.location.address + "</span><br/>";
+				if(venue.shortUrl)
+				div += "<h class='title'>Url:  </h><a target="+"'_blank'"+" href="+venue.shortUrl+">"+ venue.shortUrl + "</a><br/><br/>";
+				console.log("hhghh"+photoGroups);
+				if(photoGroups.length>1)
+					if(photoGroups[1].length!=0)
+						$.each(photoGroups[1].items,function(key,value){
 							div += "<img src='"+ value.url +"' />";
 						});
-				}
-			div += "</div>";	
-			$("#results").prepend(div);
+					else{
+						if(photoGroups.length>0)
+						if(photoGroups[0].length!=0)
+							$.each(photoGroups[0].items,function(key,value){
+								div += "<img src='"+ value.url +"' />";
+							});
+					}
+				div += "</div>";	
+				$("#results").prepend(div);
 
-		}
-	});
+			}
+		});
+	}
+	else{
+		var div = "<div class='venues'>";
+		div += "<h class='title'>" +"The user ID that you entered does not exist! Please try again with another user ID!</h><br/>";
+		div += "</div>";
+		$("#results").append(div);
+	}
+	
 }
 
 
