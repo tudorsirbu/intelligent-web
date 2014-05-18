f489bc145d44c391ad58fb8038c75dd69284cb2f
CTarta <claudiu.tarta@gmail.com>
2014-05-18 18:31:36 +0100

    Test and lib

--- src/model/OntologyTest.java
+++ src/model/OntologyTest.java
@@ -7,6 +7,7 @@ import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 
+
 public class OntologyTest {
 
 	public static void main (String args[]) {
