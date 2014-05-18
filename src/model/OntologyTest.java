e1bfca1bb8803eafcb0befd1a918bbfa08e44115
Cristian Gavrila <fcgavrila1@sheffield.ac.uk>
2014-05-18 19:36:43 +0100

    Testing the Ontology works by getting the properties from the owl file and then adding a random value which is showed in the console

--- src/model/OntologyTest.java
+++ src/model/OntologyTest.java
@@ -6,12 +6,19 @@ import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
 public class OntologyTest {
 
 	public static void main (String args[]) {
 		org.apache.log4j.BasicConfigurator.configure();
+		
+		String ns = "file:///home/cristi/workspace/intelligent-web/#";
+		
 		String filePath = "src/model/ontology.owl"; 
 		OntModel m = 
 				ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM); 
@@ -20,12 +27,30 @@ public class OntologyTest {
 		} catch (Exception e) { 
 			e.printStackTrace();
 		} 
+		
+		Resource resource = ResourceFactory.createResource(ns + "cristigavrila");
+		Property hasName = m.getOntProperty(ns + "hasName");
+		
+		Statement s = m.createStatement(resource, hasName, "Florin-Cristian Gavrila");
+		
+		m.add(s);
+		
+//		//lists all classes 
+//		ExtendedIterator<OntClass> classIterator = m.listClasses(); 
+//		while (classIterator.hasNext()) { 
+//			OntClass ontClass = classIterator.next(); 
+//			//do something with ontClass e.g. 
+//			System.out.println(ontClass.toString()); 
+//		}
+		
 		//lists all classes 
-		ExtendedIterator<OntClass> classIterator = m.listClasses(); 
+		ExtendedIterator<Resource> classIterator = m.listResourcesWithProperty(hasName); 
 		while (classIterator.hasNext()) { 
-			OntClass ontClass = classIterator.next(); 
+			Resource ontClass = classIterator.next(); 
 			//do something with ontClass e.g. 
 			System.out.println(ontClass.toString()); 
 		}
+		
+		m.write(System.out);
 	}
 }
