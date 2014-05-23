2178814de369076b10027864bcec2b1543b613ea
Tudor Sirbu <tudor.sirbu@gmail.com>
2014-05-23 20:25:19 +0100

    Added the StoplistBuilder class which reads from a text file all the words that need to be ignored

---
+++ src/util/StoplistBuilder.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class StoplistBuilder {
+
+	public ArrayList<String> getStoplist(){
+		// the list of words that should not be taken into account
+		ArrayList<String> stoplist = new ArrayList<String>();
+		
+		// get the stoplist from the text file
+		String file = "src/util/stoplist.txt";
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String line;
+		try {
+			// loop through the lines and create the stop list
+			while ((line = br.readLine()) != null) {
+			   stoplist.add(line.trim());
+			}		
+			// close the buffer
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return stoplist;
+	}
+}
