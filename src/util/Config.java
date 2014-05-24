ea2dc1d019ad51c3814ad8f88c3af93e966f5b63
Cristian Gavrila <fcgavrila1@sheffield.ac.uk>
2014-05-24 20:38:39 +0100

    Not working

--- src/util/Config.java
+++ src/util/Config.java
@@ -3,9 +3,10 @@ package util;
 public class Config {
 
 	/* RDF stuff */
-	public final static String NS = "file:///home/cristi/#";
+	public final static String NS = "file:///home/cristi/workspace/intelligent-web/#";
 	public final static String FOAF_NS = "http://xmlns.com/foaf/0.1/#";
 	public final static String GEO_NS = "http://www.w3.org/2003/01/geo/wgs84_pos#";
 	public final static String ONTOLOGY_PATH = "ontology.owl";
+	public final static String TRIPLE_STORE_PATH = "triplestore.rdf";
 	
 }
