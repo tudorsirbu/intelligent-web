9c5817752334197b71c2463e1f3b35c47a34b625
Tudor Sirbu <tudor.sirbu@gmail.com>
2014-05-25 17:57:09 +0100

    added a simulation that returns

--- src/util/RDFService.java
+++ src/util/RDFService.java
@@ -1,5 +1,13 @@
 package util;
 
+import java.util.ArrayList;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import model.User;
+import model.Venue;
+import api.TwitterManager;
+
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
@@ -31,6 +39,37 @@ public class RDFService extends RDFBase {
 	public void getUsers(long[] ids) {
 		for(long id:ids)
 			this.getUser(id);
+	}
+	
+	// simulator
+	/**
+	 * The method queries the RDF for a a venue using the provided venue name.
+	 * @param name The name of the venue to be looked up.
+	 * @return Venue the venue with that name or null
+	 */
+	public Venue getVenue(String name){
+		ArrayList<User> users  = new ArrayList<User>();
+		
+		TwitterManager twitterManager = TwitterManager.getInstance();
+		// create a connection to twitter
+		Twitter twitterConnection = null;
+		try {
+			twitterConnection = twitterManager.init();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		// get the user's id
+		try {
+			users.add(new User(twitterConnection.getUserTimeline("studor").get(0).getUser()));
+			users.add(new User(twitterConnection.getUserTimeline("cristi_gavrila").get(0).getUser()));
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new Venue("1", name, users, "In cur cu satelitul");
 	}
 	
 }
